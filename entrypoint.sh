ce3e4d0cc1d15df7bdacee6887cd924a71996da2
Yenlin Chen <3822365+hencrice@users.noreply.github.com>
2019-12-22 10:03:39 -0800

    Updated: 1. entrypoint to make it possible to test 2. release tag pattern

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
-set -euxo pipefail
+set -euo pipefail
 export PATH="$PATH:$(cd ../ && npm bin)"
-cdk --version
+
+while getopts ":ht" opt; do
+  case ${opt} in
+    h ) # show help message
+      printf "Usage: run this container without any argument to enter an interactive shell with AWS CDK pre-installed.\nOther arguments are: [-h] [-t].\nPlease refer to https://github.com/ThinkThenRant/containerized-aws-cdk for more details.\n"
+      exit 0
+      ;;
+    t ) # test the CDK installation
+      cdk --version
+      exit 0
+      ;;
+    \? )
+      printf "Usage: run this container without any argument to enter an interactive shell with AWS CDK pre-installed.\nPlease refer to https://github.com/ThinkThenRant/containerized-aws-cdk for more details.\n"
+      exit 0
+      ;;
+  esac
+done
+echo 'Entering an interactive shell with AWS CDK preinstalled. Enjoy!'
 /bin/sh
